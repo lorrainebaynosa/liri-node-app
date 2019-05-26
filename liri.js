4d83ffe31078001d7f59f9bbfa2927ccfd5a01e6
LORRAINE BAYNOSA <lorrainebaynosa@LORRAINEs-MacBook-Pro.local>
2019-05-26 11:54:57 -0700

    working need to clean it up and add moment.js

--- liri.js
+++ liri.js
@@ -6,9 +6,6 @@ require("dotenv").config();
 // npm init
 // npm install dotenv: package.json will now have dotenv listed as a dependency
 
-
-
-console.log(keys);
 // testing by entering into terminal: 
 // node liri.js
 // RETURNS: 
@@ -33,25 +30,28 @@ var Spotify = require("node-spotify-api");
 // Load the fs package to read and write
 var fs = require('fs');
 var action = process.argv[2];
-var artist = process.argv[3];
-// SPOTIFY VAR
-var song = process.argv[3];
-// OMDB API:
-var movieName = process.argv[3];;
+console.log('keys', keys);
+var cmdArgument = process.argv[3];
 
 // Node application called liri.js will take in in user inputs via the command line to register actions/commands.
 // The actions/commands possible are:
-// "concert-this": see detailed pseudocode below:
-// "spotify-this-song": see detailed pseudocode below:
-// "movie-this": see detailed pseudocode below:
+// "concert-this"
+// "spotify-this-song"
+// "movie-this"
 // "do-what-it-says"
 
+// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+//   * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
+
+//   * It's on Netflix!
+console.log('process.argv', process.argv)
+
 if (action === "concert-this") {
-    concertThis();
+    concertThis(cmdArgument);
 } else if (action === "spotify-this-song") {
-    spotifyThisSong();
+    spotifyThisSong(cmdArgument);
 } else if (action === "movie-this") {
-    movieThis();
+    movieThis(cmdArgument || 'Mr.Nobody');
 } else if (action === "do-what-it-says") {
     doWhatItSays();
 } else {
@@ -64,17 +64,34 @@ if (action === "concert-this") {
 // * Date of the Event (use moment to format this as "MM/DD/YYYY")
 // Then run a request with axios to the Banks in Town Artist Events API with artist specified
 
-function concertThis() {
+function concertThis(artist) {
+    // if artist has multiple words, encodeURI component, sample run with API documentation on 
+    // https://app.swaggerhub.com/apis-docs/Bandsintown/PublicAPI/3.0.0#/artist%20events/artistEvents, testing: Maroon 5 as artist: request URL returns:
+    // https://rest.bandsintown.com/artists/Maroon%205/events?app_id=40dec023-122f-4fa0-8440-0ef843692fbe
+
     axios
-        .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=40dec023-122f-4fa0-8440-0ef843692fbe")
+        // .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=40dec023-122f-4fa0-8440-0ef843692fbe")
+        .get("https://rest.bandsintown.com/artists/" + encodeURIComponent(artist) + "/events?app_id=40dec023-122f-4fa0-8440-0ef843692fbe")
         .then(function (response) {
-            console.log(response.data);
-            console.log(typeof response);
-            var results = JSON.parse(response);
-            // parse a string (written in JSON format) to return JavaScript object:
-            console.log("Name of the Venue: " + results.venue.name);
-            console.log("Venue location: " + results.venue);
-            console.log("Date of the Event: " + results.datetime);
+            // console.log('response', response);
+            // console.log('typeof response', typeof response);
+            const results = response.data;
+            if (results.length) {
+                console.log('has results', results);
+                //loop through it
+            } else {
+                console.log(`no events for artist`)
+            }
+            // assuming there is >= 1 results for each artist
+            // render the following information about each event to the terminal:
+            // Name of the venue 
+            // Venue location
+            // Date of the Event (use moment to format this as "MM/DD/YYYY")
+            for (i = 0; i <results.length; i ++) {
+                console.log("Name of Venue: " + results[i].venue.name);
+                console.log("Venue location: " + results[i].venue);
+                console.log("Date of the Event: " + results[i].datetime);
+            }
         })
         .catch(function (error) {
             if (error.response) {
@@ -108,13 +125,13 @@ function concertThis() {
 // console.log(data.album);
 // https://www.npmjs.com/package/node-spotify-api
 
-function spotifyThisSong() {
+function spotifyThisSong(query) {
     var spotify = new Spotify(keys.spotify);
     spotify.search({ type: "track", query: "Shallow" }, function (err, data) {
         if (err) {
             return console.log("Error occurred: " + err);
         }
-        console.log(data);
+        console.log('spotify data', data.tracks.items);
     });
 }
 
@@ -129,9 +146,14 @@ function spotifyThisSong() {
 //     * Actors in the movie.
 //   ```
 
-function movieThis() {
+function movieThis(movieName) {
     // for movie with multiple words, split by spaces: split() method splits a String object into an array of strings by separating the string into substrings, using a specified separator string to determine where to make each split. In this case, the specified separator is a space.
     // NOTE: To split a string with commas, write: .split(",");
+    console.log('movieName in movieThis', movieName)
+    if (!movieName) {
+        console.log('No movie name provided')
+        return undefined
+    }
 
     var movieArr = movieName.split(" ").join("+");
     console.log(movieArr);
@@ -150,22 +172,18 @@ function movieThis() {
             console.log("Title of Movie: " + response.data.Title);
             console.log("Year Movie Came Out: " + response.data.Year);
             console.log("IMDB Rating: " + response.data.imdbRating);
-            // console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1]; 
-            // // for (var key in response.data.Ratings){
-            //     console.log(key, response.data.Ratings;
-            // }
+            console.log(response.data.Ratings[1]);
+            // test Rotten Tomatoes Rating
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
             console.log("Country where movie was produced: " + response.data.Country);
             console.log("Language of movie: " + response.data.Language);
             console.log("Plot of movie: " + response.data.Plot);
             console.log("Actors in movie: " + response.data.Actors);
+            // * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+//   * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/> . It's on Netflix!
         }
     );
 }
-// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-//   * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-//   * It's on Netflix!
 
 function doWhatItSays() {
     // This block of code will read from the "random.txt" file.Include "utf8" parameter or the code will provide stream data (garbage)
@@ -187,12 +205,12 @@ function doWhatItSays() {
         console.log(dataArr);
         console.log(dataArr[0]);
         console.log(dataArr[1]);
-        if (dataArr[0]=== "spotify-this-song"){
+        if (dataArr[0] === "spotify-this-song") {
             spotifyThisSong(dataArr[1]);
         } else if (dataArr[0] === "concert-this") {
             concertThis(dataArr[1]);
         } else if (dataArr[0] === "movie-this") {
-            movieThis();
+            movieThis(dataArr[1]);
         } else {
             console.log("This is not a recognized command")
         }
